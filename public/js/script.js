9f480a543b443c8c59d9908c81775354f1a079f3
Nathan Dev <nathandev33@gmail.com>
2022-07-04 20:59:21 +0200

    .

--- public/js/script.js
+++ public/js/script.js
@@ -79,27 +79,27 @@ async function SaveToAnki() {
 
 // NO WHITE SCREEN when rendering iframe
 // Prevent variables from being global
-(function () {
-  /*
-          1. Inject CSS which makes iframe invisible
-          */
+// (function () {
+//   /*
+//           1. Inject CSS which makes iframe invisible
+//           */
 
-  var div = document.createElement("div"),
-    ref =
-      document.getElementsByTagName("base")[0] ||
-      document.getElementsByTagName("script")[0];
+//   var div = document.createElement("div"),
+//     ref =
+//       document.getElementsByTagName("base")[0] ||
+//       document.getElementsByTagName("script")[0];
 
-  div.innerHTML = "&shy;<style> iframe { visibility: hidden; } </style>";
+//   div.innerHTML = "&shy;<style> iframe { visibility: hidden; } </style>";
 
-  ref.parentNode.insertBefore(div, ref);
+//   ref.parentNode.insertBefore(div, ref);
 
-  /*
-        2. When window loads, remove that CSS, 
-        making iframe visible again
-        */
+//   /*
+//         2. When window loads, remove that CSS,
+//         making iframe visible again
+//         */
 
-  window.onload = function () {
-    div.parentNode.removeChild(div);
-    document.getElementById("slovo").focus();
-  };
-})();
+//   window.onload = function () {
+//     div.parentNode.removeChild(div);
+//     document.getElementById("slovo").focus();
+//   };
+// })();
