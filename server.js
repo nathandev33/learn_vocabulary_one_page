6f34c8a6f67b860b155c5d5debe7fc01cf5def92
Nathan Dev <nathandev33@gmail.com>
2022-07-04 19:31:25 +0200

    packages added

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
+import compression from "compression";
 dotenv.config();
 import cors from "cors";
 const app = express();
@@ -12,7 +17,19 @@ const __dirname = path.dirname(__filename);
 import getSlovnik from "./controllers/fetchSeznam.js";
 
 // MIDDLEWARES
+
+app.use(helmet());
+app.use(compression());
+app.use(mongoSanitize());
+app.use(xss());
+const limiter = rateLimit({
+  max: 300,
+  windowMs: 60 * 60 * 1000,
+  message: "too many request from this ip please try again in an hour",
+});
+// app.use("/api", limiter);
 app.use(cors());
+
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
